0641ec2da2b49d4f0ab30ce8f5c7d65b96a3db3c
Petro <petroarva33@gmail.com>
2025-02-27 15:45:22 +0100

    feat: update error page text color

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,27 +1,28 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { ROUTES } from "../routes/routes";
+import CustomButton from "../components/CustomButton";
 
 const ErrorPage = () => {
+  const navigate = useNavigate();
   return (
     <section>
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-        <div className="mx-auto max-w-screen-sm text-center">
-          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
+        <div className="mx-auto max-w-screen-sm text-center flex flex-col items-center">
+          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-grey">
             404
           </h1>
-          <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+          <p className="mb-4 text-3xl tracking-tight font-bold text-grey md:text-4xl dark:text-grey">
             Something's missing.
           </p>
-          <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
+          <p className="mb-4 text-lg font-light text-grey dark:text-grey">
             Sorry, we can't find that page. You'll find lots to explore on the
             home page.{" "}
           </p>
-          <Link
-            to={ROUTES.DASHBOARD}
-            className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-1 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
-          >
-            Back to Homepage
-          </Link>
+          <CustomButton
+            text="Back to Homepage"
+            onClick={() => navigate(ROUTES.DASHBOARD)}
+            backgroundColor="primary"
+          />
         </div>
       </div>
     </section>
