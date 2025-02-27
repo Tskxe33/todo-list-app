40e4f3fa0ed310b818f9ad582f52966fac689c14
Petro <petroarva33@gmail.com>
2025-02-27 14:18:34 +0100

    feat: add toastify when reordering was successful

--- src/components/sortable/SortableList.tsx
+++ src/components/sortable/SortableList.tsx
@@ -1,7 +1,7 @@
 import { ReactSortable } from "react-sortablejs";
 import { Todo } from "../../models/todo.model";
 import SortableItem from "./SortableItem";
-
+import successNotification from "../../utils/toastify/success";
 interface Props {
   items: Todo[];
   setItems: (items: Todo[]) => void;
@@ -14,6 +14,10 @@ const SortableList = ({ items, setItems }: Props) => {
       handle=".handle"
       setList={setItems}
       className="flex flex-col gap-6 justify-center items-center"
+      onEnd={(e) =>
+        e.oldIndex !== e.newIndex &&
+        successNotification("Reordered successfully 🎉")
+      }
     >
       {items.map((item, index) => (
         <SortableItem key={item.id} item={item} index={index + 1} />
