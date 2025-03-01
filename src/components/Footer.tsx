74501fdba38034f38aebf707c7fd3b0b89d00537
Petro <petroarva33@gmail.com>
2025-03-01 14:17:27 +0100

    feat: unit tests footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="flex items-center justify-center mt-4">
+    <div className="flex items-center justify-center mt-4" data-testid="footer">
       <p className="text-xs text-gray-400 my-2">
         All rights reserved &copy; {new Date().getFullYear()} Petro Arva
       </p>
